09dd14ead6870cf168047ed86f7c2e857011f08b
Eladzy <yacovski551@gmail.com>
2020-01-05 08:49:57 +0200

    Update ViewModel.cs

--- FlightProject/DbGenerator/FlightDataBaseFiller/FlightDataBaseFiller/ViewModel/ViewModel.cs
+++ FlightProject/DbGenerator/FlightDataBaseFiller/FlightDataBaseFiller/ViewModel/ViewModel.cs
@@ -4,12 +4,30 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Threading;
+using Prism;
+using Prism.Commands;
 
 namespace FlightDataBaseFiller.ViewModel
 {
     class ViewModel
     {
         Dispatcher dispatcher { get; set; }
+        public DelegateCommand Command { get; set; }
+        private DispatcherTimer Timer=new DispatcherTimer();
+        public ViewModel()
+        {
+            
+            this.Timer.Interval = TimeSpan.FromMilliseconds(500);
+            this.Timer.Tick += VerifyValues;
+            this.Timer.Start();
+            Command = new DelegateCommand(ExecuteCommand, CanExecuteCommand);
+        }
 
+       
+
+        private void VerifyValues(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
