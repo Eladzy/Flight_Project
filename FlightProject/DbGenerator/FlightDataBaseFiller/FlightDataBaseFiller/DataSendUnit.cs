81b1914930f067fefcaf6fd4c5dbe4fd6f582665
Eladzy <45952716+Eladzy@users.noreply.github.com>
2020-01-25 21:53:21 +0200

    1

---
+++ FlightProject/DbGenerator/FlightDataBaseFiller/FlightDataBaseFiller/DataSendUnit.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FlightManagerProject;
+
+namespace FlightDataBaseFiller
+{
+    class DataSendUnit
+    {
+        //might want to add admin log on
+    }
+}
