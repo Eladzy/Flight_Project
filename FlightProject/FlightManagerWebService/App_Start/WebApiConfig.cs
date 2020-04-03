03c1c49a33447e0db2943bcd9de3020d7fc74af7
Eladzy <yacovski551@gmail.com>
2020-04-03 11:59:22 +0300

    enabled cors

--- FlightProject/FlightManagerWebService/App_Start/WebApiConfig.cs
+++ FlightProject/FlightManagerWebService/App_Start/WebApiConfig.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
+using System.Web.Http.Cors;
 
 namespace FlightManagerWebService
 {
@@ -10,16 +11,20 @@ namespace FlightManagerWebService
         public static void Register(HttpConfiguration config)
         {
             // Web API configuration and services
-            //enable cors
-            config.EnableCors();
+            
             // Web API routes
             config.MapHttpAttributeRoutes();
+            //Web API routes
+           // config.MessageHandlers.Add(new TokenValidationHandler());
 
             config.Routes.MapHttpRoute(
                 name: "DefaultApi",
                 routeTemplate: "api/{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
+            //enable cors
+            EnableCorsAttribute cors = new EnableCorsAttribute("*", "*", "*");
+            config.EnableCors(cors);
         }
     }
 }
