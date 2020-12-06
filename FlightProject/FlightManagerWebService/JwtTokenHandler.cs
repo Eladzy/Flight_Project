f943f0256b937c8b0f01b43e4d4d5ae0627c6bd0
Eladzy <yacovski551@gmail.com>
2020-12-06 07:25:34 +0200

    added comments removed unused files

--- FlightProject/FlightManagerWebService/JwtTokenHandler.cs
+++ FlightProject/FlightManagerWebService/JwtTokenHandler.cs
@@ -14,7 +14,14 @@ namespace FlightManagerWebService
 {
     public class JwtTokenHandler:DelegatingHandler
     {
-
+        /// <summary>
+        /// validates token is not expired
+        /// </summary>
+        /// <param name="notBefore"></param>
+        /// <param name="expires"></param>
+        /// <param name="securityToken"></param>
+        /// <param name="validationParameters"></param>
+        /// <returns></returns>
         public bool LifetimeValidator(DateTime? notBefore,DateTime? expires,SecurityToken securityToken,TokenValidationParameters validationParameters)
         {
             if (expires != null)
@@ -24,12 +31,17 @@ namespace FlightManagerWebService
             return false;
         }
 
-
+        /// <summary>
+        /// verifies that the request includes Jwt token and outs a trimmed jwt token
+        /// </summary>
+        /// <param name="request"></param>
+        /// <param name="token"></param>
+        /// <returns></returns>
         private static bool TryGetToken(HttpRequestMessage request,out string token)
         {
             token = null;
             IEnumerable<string> authzHeaders;
-            if (!request.Headers.TryGetValues("Authorization", out authzHeaders) || authzHeaders.Count() > 1)
+            if (!request.Headers.TryGetValues("Authorization", out authzHeaders) || authzHeaders.Count() > 1)//if cannot retrieve auth values or values length<1 returns false
             {
                 return false;
             }
@@ -47,10 +59,10 @@ namespace FlightManagerWebService
             if(!TryGetToken(request,out token))
             {
                 statusCode = HttpStatusCode.Unauthorized;
-               return  base.SendAsync(request, cancellationToken);
+               return  base.SendAsync(request, cancellationToken);//unauthorized token would yield cancellation token
             }
             try
-            {
+            {//decodes the request message
                 const string secretKey = "99C4A955E7274BE9B4D78B0025E04E88D55C6783EF3446C88ACAF9EBC22D9758";
                 var securityKey = new SymmetricSecurityKey(System.Text.Encoding.Default.GetBytes(secretKey));
 
@@ -68,7 +80,7 @@ namespace FlightManagerWebService
                     IssuerSigningKey = securityKey
                 };
 
-                //extract and assign the user of the jwt gets the claims
+                //extract and assign the user of the jwt gets the claims settings
                 Thread.CurrentPrincipal = handler.ValidateToken(token, validationParameters, out _);
                 HttpContext.Current.User = handler.ValidateToken(token, validationParameters, out _);
 
