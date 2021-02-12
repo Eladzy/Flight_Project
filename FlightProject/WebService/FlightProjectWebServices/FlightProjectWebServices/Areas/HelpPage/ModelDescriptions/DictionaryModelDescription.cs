1bab5a3ccbb9cd52d2ea7b941dafe1e95175ed47
Eladzy <45952716+Eladzy@users.noreply.github.com>
2021-02-12 06:24:54 +0200

    Delete FlightProject/WebService/FlightProjectWebServices directory

--- FlightProject/WebService/FlightProjectWebServices/FlightProjectWebServices/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace FlightProjectWebServices.Areas.HelpPage.ModelDescriptions
-{
-    public class DictionaryModelDescription : KeyValuePairModelDescription
-    {
-    }
-}
--- FlightProject/WebService/FlightProjectWebServices/FlightProjectWebServices/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace FlightProjectWebServices.Areas.HelpPage.ModelDescriptions
-{
-    public class SimpleTypeModelDescription : ModelDescription
-    {
-    }
-}
